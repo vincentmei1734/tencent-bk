3016a8bef9efdbd3c2a4650b0a343e4b3c3cb13c
vincent.mei1734@gmail.com <vincent.mei1734@gmail.com>
2019-07-14 07:52:33 +0800

    modified:   conf/settings_development.py 	modified:   conf/settings_production.py 	modified:   conf/settings_testing.py

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -8,13 +8,20 @@ from settings import APP_ID
 # ===============================================================================
 # 数据库设置, 正式环境数据库设置
 # ===============================================================================
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
+#         'NAME': '',                            # 数据库名 (默认与APP_ID相同)
+#         'USER': '',                            # 你的数据库user
+#         'PASSWORD': '',                        # 你的数据库password
+#         'HOST': '',                   		   # 数据库HOST
+#         'PORT': '',                            # 默认3306
+#     },
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': '',                            # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
-        'PORT': '',                            # 默认3306
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -9,13 +9,20 @@ from settings import APP_ID
 # 数据库设置, 测试环境数据库设置
 # ===============================================================================
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
+#         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+#         'USER': 'test',                            # 你的数据库user
+#         'PASSWORD': '123456',                        # 你的数据库password
+#         'HOST': '10.0.1.102',                            # 开发的时候，使用localhost
+#         'PORT': '',                            # 默认3306
+#     },
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'test',                            # 你的数据库user
-        'PASSWORD': '123456',                        # 你的数据库password
-        'HOST': '10.0.1.102',                            # 开发的时候，使用localhost
-        'PORT': '',                            # 默认3306
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
