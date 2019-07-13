ecb340cdd11413478c8984e4db044ee3df7f0976
vincent.mei1734@gmail.com <vincent.mei1734@gmail.com>
2019-07-13 19:42:59 +0800

    modified:   conf/settings_testing.py

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -8,13 +8,14 @@ from settings import APP_ID
 # ===============================================================================
 # 数据库设置, 测试环境数据库设置
 # ===============================================================================
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': '',                            # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                		       # 数据库HOST
+        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'test',                            # 你的数据库user
+        'PASSWORD': '123456',                        # 你的数据库password
+        'HOST': '10.0.1.102',                            # 开发的时候，使用localhost
         'PORT': '',                            # 默认3306
     },
 }
